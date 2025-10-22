64b599cbe4aa818cf49d9187a1829e969789c3ee
anithaaluru <info@devops.com>
2025-10-22 20:01:25 +0530

    roboshop

--- 1-roboshop.sh
+++ 1-roboshop.sh
@@ -6,7 +6,7 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 for instance in {$INSTANCES[@]}
 do
   INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-groups sg-01fb8f67c6dc9cd19 --output text) 
-    if [ $instance != frontend ]
+    if [ $instance != "frontend" ]
      then
       IP=$(aws ec2 describe-instances $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
