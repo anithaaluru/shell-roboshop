0a03a179392351d1661354174572c9700aa94abb
anithaaluru <info@devops.com>
2025-10-22 20:42:40 +0530

    roboshop

--- 1-roboshop.sh
+++ 1-roboshop.sh
@@ -3,9 +3,9 @@ AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-01fb8f67c6dc9cd19"
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping"  "payment" "dispatch" "frontend")
 
-for instance in {$INSTANCES[*]}
+for instance in {$INSTANCES[@]}
 do
-  INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-01fb8f67c6dc9cd19 --tag-specifications"ResourceType=instance,Tags=[{key=Name, Value=test}]" --query "Instances[0].InstanceId" --output text) 
+  INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-01fb8f67c6dc9cd19 --tag-specifications "ResourceType=instance,Tags=[{key=Name, Value=test}]" --query "Instances[0].InstanceId" --output text) 
     if [ $instance != "frontend" ]
      then
       IP=$(aws ec2 describe-instances $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
