70c21f5aa2b9dad753f57336f4fadf99d4786cb9
anithaaluru <info@devops.com>
2025-10-22 20:28:29 +0530

    roboshop

--- 1-roboshop.sh
+++ 1-roboshop.sh
@@ -3,7 +3,7 @@ AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-01fb8f67c6dc9cd19"
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping"  "payment" "dispatch" "frontend")
 
-for instance in {$INSTANCES[@]}
+for instance in "{$INSTANCES[@]}"
 do
   INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-groups sg-01fb8f67c6dc9cd19 --output text) 
     if [ $instance != "frontend" ]
