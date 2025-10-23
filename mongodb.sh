7cf8f73ba0062cff768ea29724a9a1805bf44cbd
anithaaluru <info@devops.com>
2025-10-23 17:37:17 +0530

    roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -13,6 +13,7 @@ echo "script executed at: $(date)" | tee -a $LOG_FILE
 if [ $USER_ID -ne 0 ]
  then
   echo -e "$R ERROR::please run this script with root  access $N" | tee -a $LOG_FILE
+  exit 1
  else
   echo -e "$Y you are running this script with root access $N" | tee -a $LOG_FILE
 fi
