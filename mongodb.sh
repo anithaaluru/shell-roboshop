fb9e9ba335a974c50f8f6498f91458b43092ec5b
anithaaluru <info@devops.com>
2025-10-23 17:22:38 +0530

    roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -22,6 +22,7 @@ fi
    if [ $1 -ne 0 ]
     then
      echo -e "$2 is...... $R failure $N" | tee -a $LOG_FILE
+     exit 1
    else
      echo -e "$2 is.... $G success $N" | tee -a $LOG_FILE
    fi
