f97d2d8182b8881b16aa00cf0738b2cde9fc849f
anithaaluru <info@devops.com>
2025-10-24 11:51:43 +0530

    roboshop

--- 1-roboshop.sh
+++ 1-roboshop.sh
@@ -26,7 +26,7 @@ do
       ,"Changes": [ { 
       "Action"            : "UPSERT" 
       ,"ResourceRecordSet": { 
-      "Name"              : "'$DOMAIN_NAME'" 
+      "Name"              : "'$instance'.'$DOMAIN_NAME'" 
       ,"Type"             : "A"
       ,"TTL"              : 1
       ,"ResourceRecords"  : [{ 
