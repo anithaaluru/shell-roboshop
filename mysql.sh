9485a1c411b71ce76acb8374d67822d8ce8737d4
anithaaluru <info@devops.com>
2025-10-25 20:13:34 +0530

    roboshop

--- mysql.sh
+++ mysql.sh
@@ -24,15 +24,15 @@ fi
   VALIDATE(){
    if [ $1 -ne 0 ]
     then
-     echo -e "$2 is...... $R failure $N" &>>LOG_FILE
+     echo -e "$2 is...... $R failure $N" | tee -a $LOG_FILE
    else
-     echo -e "$2 is.... $G success $N" &>>LOG_FILE
+     echo -e "$2 is.... $G success $N" | tee -a $LOG_FILE
    fi
   }
   dnf install mysql-server -y &>>LOG_FILE
   VALIDATE $? "installing mysql"
-  systemctl enable mysqld
+  systemctl enable mysqld &>>LOG_FILE
   systemctl start mysqld &>>LOG_FILE 
   VALIDATE $? "starting mysql" 
-  mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD
+  mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD &>>LOG_FILE
   VALIDATE $? "setting root password"
